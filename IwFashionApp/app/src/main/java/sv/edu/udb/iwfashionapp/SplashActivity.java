ee515b190e216d9fce0f4bf49aafafbcb9f15c53
Josue Villeda <josuechacon46@gmail.com>
2021-04-26 21:08:07 -0600

    Llamado al Splash como primer pantalla, luego login

--- IwFashionApp/app/src/main/java/sv/edu/udb/iwfashionapp/SplashActivity.java
+++ IwFashionApp/app/src/main/java/sv/edu/udb/iwfashionapp/SplashActivity.java
@@ -2,33 +2,35 @@ package sv.edu.udb.iwfashionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.os.Handler;
 import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
-public class SplashGifActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
+
     ImageView gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_gif);
+        setContentView(R.layout.activity_splash);
+
         gif = findViewById(R.id.gif);
 
-        /*Glide.with(this).load("https://firebasestorage.googleapis.com/v0/b/gifiwfashion.appspot.com/o/gifprueba.gif?alt=media&token=281a1958-db90-4a68-a928-304d86f3c3af").asGif().diskCacheStrategy(DiskCacheStrategy.ALL).fitCenter().into(gif);
+        Glide.with(this).load("https://firebasestorage.googleapis.com/v0/b/gifiwfashion.appspot.com/o/gifprueba.gif?alt=media&token=281a1958-db90-4a68-a928-304d86f3c3af").diskCacheStrategy(DiskCacheStrategy.ALL).fitCenter().into(gif);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashGifActivity.this,Login.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                startActivity(new Intent(SplashActivity.this, Login.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
                 overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
                 //Finish
                 finish();
 
             }
-        }, 2000);*/
+        }, 2000);
+
     }
 }
